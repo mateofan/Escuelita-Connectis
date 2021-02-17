ba1cae704141cbbe7dcdb6c3208bd59887b19fdb
mateofan <mateofan1999@gmail.com>
2021-02-17 15:03:00 -0300

    First Commit

---
+++ Cypress Course/cypress/integration/movistar.test.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+describe('movistar', ()=>{
+
+    beforeEach(()=>{
+        cy.visit('https://tienda.movistar.com.ar');
+    })
+
+    it('A10s con cuotas', ()=>{
+        var cantCuotas = 12;
+        cy.get('.icon-search.pnt-js-boton-buscador').type(' ');
+        cy.get('#myInput').type('A10s{enter}');
+        cy.get('.button').click()
+        cy.get('.details > ul > :nth-child(1)').contains('cuotas').invoke('text').should((text1)=>{
+            var res = text1.split(" ");
+            var cuotas = parseInt(res[1])
+            if(cuotas > 12){
+                cantCuotas = cuotas
+            }
+
+            if(text1 != `Hasta ${cantCuotas} cuotas sin interés con tarjetas seleccionadas`){
+                throw new Error('El producto no soporta un minimo de 12 cuotas sin interes');
+            }
+
+        })
+        
+    })
+    it('Alta Gama', ()=>{
+        cy.get('li > .btn-group > .btn-filter').click();
+        cy.contains('256GB').click();
+        cy.get('li > .btn-group > .btn-filter').click();
+        cy.contains('Gama Alta').click();
+
+    })
+
+})
