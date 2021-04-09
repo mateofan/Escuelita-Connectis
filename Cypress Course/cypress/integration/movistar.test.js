259018cda9ae2be5d7d17dc1d4605ad3fd29a1b6
mateofan <mateofan1999@gmail.com>
2021-04-09 13:33:36 -0300

    Ejercicios capacitacion marzo

--- Cypress Course/cypress/integration/movistar.test.js
+++ Cypress Course/cypress/integration/movistar.test.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
 const MovistarPage = require("../pageObjects/movistarPage");
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
+  
+
 
 describe('movistar', ()=>{
     const movistarPage = new MovistarPage();
@@ -9,70 +16,45 @@ describe('movistar', ()=>{
         cy.visit('https://tienda.movistar.com.ar');
     })
 
-    it('CP001-Validar cuotas en compra de equipo -Cuotas.12 -Equipo.A31', ()=>{
-
-        movistarPage.getBusqueda().type(' ');
-        movistarPage.getCampoBusqueda().type('A31{enter}');
-        movistarPage.getProductName().contains('Galaxy A31').click();
-        movistarPage.getTextoCuotas().should('contain','18 cuotas')
-        
-        
-    })
-    it('CP002-Aplicar filtro de equipos -Gama.Alta -Memoria Interna.256GB  //Una sola pagina', ()=>{
-        movistarPage.getFiltros().click();
-        cy.contains('256GB').click();
-        movistarPage.getFiltros().click();
-        cy.contains('Gama Alta').click();
-        movistarPage.getProductos().then((res)=>{
-            movistarPage.getCantProductos().invoke('text').then((t)=>{
-                var text = t.split(" ");
-                var phonesFound = parseInt(text[0]);
-                expect(res[0].children).to.have.lengthOf(phonesFound);
-
-            })
-            
-        })
-    })
-    it('CP003-Validar cuotas en compra de equipo -Cuotas.60 -Equipo.Tercero de la lista -Banco.Credicoop -Tarjeta.Visa', ()=>{
-        movistarPage.getProducto().eq(2).click();
-        movistarPage.getFinanciacion().click();
-        movistarPage.getBanco().select('Credicoop');
-        movistarPage.getTarjeta().select('Visa');
-        movistarPage.getTablaCuotas().within((elem)=>{
-            var table= elem[0];
-            for(var i=0; i<table.children.length;i++){
-                expect(table.children[i].cells[0].innerText).to.not.equal('60')
-            }
-        })
-    }) 
-    it('CP004-Revisar si puedo contratar internet', ()=>{
-
-        /*
-        Descripción: El objetivo es visitar la tienda de movistar, luego ingresar
-        a la parte de productos y servicios e ingresar a internet, seleccionar el segundo plan y
-        elegir 'No tengo linea', completar una direccion y poner siguiente.
-        Resultado esperado:
-        -Que se pueda ingresar a la pagina
-        -Verificar que el url sea correcto
-        -Que de una respuesta sobre la direccion seleccionada
-
-        */
+    it('Planes de TV', ()=>{
 
         movistarPage.getProductosYServicios().trigger("mouseover");
-        movistarPage.getServicio().contains('Internet').click();
-        movistarPage.getBotonPlan().eq(1).click();
-        cy.url().should('eq', 'https://ventas.movistar.com.ar/productos/')
-        movistarPage.getTipoPlan().eq(1).click();
-        movistarPage.getLocalidad().type('Av. Córdoba 1646{downarrow}{enter}').type('{downarrow}')
-        //Falta solucionar problema con el autocomplete
-        movistarPage.getContratar().click();
-        cy.get('body').then(($body)=>{
-            if($body.find("#offert-list").length > 0){
-                cy.log("=== Prueba superada ===")
-            }else{
-                throw new Error ("No es posible contratar el plan")
+        movistarPage.getServicio().contains('Planes de TV').click();
+        movistarPage.checkTelevision();
+        var planes = [];
+        var lengthPreciosDescontados = 0
+        movistarPage.getPrecioDescontado().each(($el, index, $list)=>{
+            lengthPreciosDescontados++;
+            var text = $el[0].innerText;
+            var res = text.replace(/\D/g, "");
+            planes.push({
+                precioDescontado : res
+            })
+    
+        })
+        movistarPage.getPrecioFinal().each(($el, index, $list)=>{
+            
+            var text = $el[0].innerText;
+            var res = text.replace(/\D/g, "");
+            planes[index] = {
+                precioDescontado : planes[index].precioDescontado,
+                precioFinal : res
+            }
+        }).then(($list)=>{
+            expect(lengthPreciosDescontados).to.equal(planes.length); //Para evitar el hipotetico caso que este mal cargado un plan sin descuento
+            expect(planes.length).to.be.at.least(3);
+            for(let i = 0; i < planes.length; i++){
+            cy.log(`Plan numero ${i+1} valor descontado: ${planes[i].precioDescontado} valor final: ${planes[i].precioFinal}`)
             }
         })
+
+
     })
+
+    /*it('Negocios', ()=>{
+
+        cy.get('.navigation-secondary__item').eq(1).click()
+    })*/
+    
 
 })
